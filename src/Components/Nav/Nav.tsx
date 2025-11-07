d15120fa39b91b4aa1c0d66209382ee6c5f9e303
Stefano Pisano <s.pisano91@gmail.com>
2025-11-07 19:56:16 +0100

    feat: add nav links

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -9,8 +9,16 @@ const Nav:React.FC = () => {
             <div className={"w-full h-20 flex flex-row p-2 bg-yimin-blue"}>
                 <img src={logo} className={"h-full"}/>
 
-                <div className={"flex flex-auto justify-end items-center align pr-2"}>
-                    <LanguageSelector/>
+                <div className={"flex flex-auto items-center pr-2"}>
+                    <div className={"w-full flex gap-2 flex-auto items-center justify-center"}>
+                        <a href={""}>Lorem Ipsum Link 1</a>
+                        <a href={""}>Lorem Ipsum Link 2</a>
+                        <a href={""}>Lorem Ipsum Link 3</a>
+                    </div>
+
+                    <div className={"flex flex-auto justify-end"}>
+                        <LanguageSelector/>
+                    </div>
                 </div>
             </div>
         </>
